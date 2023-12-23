f9e81926ee244e18b9963aef3602b2dad48682da
YTDev <belnisay@gmail.com>
2023-12-23 01:38:15 +0000

    forgot js err

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
 document.getElementById('uploadForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var formData = new FormData(this);
-
+    const apiToken = document.getElementById('apiToken').value;
+    formData.append('apiToken', apiToken);
     fetch('/upload', {
         method: 'POST',
         body: formData
