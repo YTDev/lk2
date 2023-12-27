59e19f192617f134b9ce4b203fb143bdc0567ef2
YTDev <belnisay@gmail.com>
2023-12-27 17:13:18 +0000

    fix

--- public/script.js
+++ public/script.js
@@ -1,42 +1,43 @@
-
 // Disable auto discover for all elements:
 Dropzone.autoDiscover = false;
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Create a Dropzone for the #imageDropzone element
+    // Initialize Dropzone
     var myDropzone = new Dropzone("#imageDropzone", {
         url: "/upload",
         autoProcessQueue: false,
         addRemoveLinks: true,
         paramName: "image", // The name that will be used to transfer the file
+        parallelUploads: 10, // Adjust as per your requirements
+        uploadMultiple: true, // Allow multiple file uploads
+        maxFiles: 10, // Adjust the maximum number of files here
+        // You can add more options as required
     });
 
-    // Handle the form submission
+    // Add the API token to Dropzone's formData
+    myDropzone.on("sending", function(file, xhr, formData) {
+        var apiTokenInput = document.getElementById('apiTokenInput');
+        formData.append("apiToken", apiTokenInput.value);
+    });
+
+    // Handle form submission
     document.getElementById("uploadForm").addEventListener("submit", function(event) {
         event.preventDefault();
-        
-        // Append the API Token to Dropzone's formData
-        var apiTokenInput = document.getElementById('apiTokenInput');
-        myDropzone.on("sending", function(file, xhr, formData) {
-            formData.append("apiToken", apiTokenInput.value);
-        });
+        myDropzone.processQueue(); // Process the uploaded files
+    });
 
-        // Process the queue when the form is submitted
-        myDropzone.processQueue();
-
-        // Listen for the "complete" event on all files
-        myDropzone.on("queuecomplete", function() {
-            // Your existing code to fetch '/upload' and handle the response
-            fetch('/upload', {
-                method: 'POST',
-                body: myDropzone.getAcceptedFiles() // Send the accepted files
-            })
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('responseContainer').innerHTML = 
-                    `<p>Images uploaded. Printify responses: ${JSON.stringify(data.printifyResponses)}</p>`;
-            })
-            .catch(error => console.error('Error:', error));
-        });
+    // Handle the response after all files have been uploaded
+    myDropzone.on("queuecomplete", function() {
+        fetch('/upload', {
+            method: 'POST',
+            body: myDropzone.getAcceptedFiles()
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('responseContainer').innerHTML = 
+                `<p>Images uploaded. Printify responses: ${JSON.stringify(data.printifyResponses)}</p>`;
+        })
+        .catch(error => console.error('Error:', error));
+        myDropzone.removeAllFiles(); // Clear the Dropzone area after upload
     });
 });
