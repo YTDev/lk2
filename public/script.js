8323d55f1108b9e90c2e9a57527dc02be356a8fc
YTDev <belnisay@gmail.com>
2023-12-29 23:18:57 +0000

    fix

--- public/script.js
+++ public/script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const pond = FilePond.create(document.getElementById('imageInput'), {
+        instantUpload: false,
         server: {
             process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
                 const formData = new FormData();
                 formData.append(fieldName, file, file.name);
+                
+                // Append the API token from the input field to the FormData
                 formData.append('apiToken', document.getElementById('apiTokenInput').value);
 
                 const request = new XMLHttpRequest();
@@ -12,21 +15,34 @@ document.addEventListener('DOMContentLoaded', function() {
                     if (request.status >= 200 && request.status < 300) {
                         load(request.responseText);
                     } else {
-                        error('oh no');
+                        error('Upload failed');
                     }
                 };
+                request.onerror = function() {
+                    error('Upload failed');
+                };
                 request.send(formData);
+                
                 return {
                     abort: () => {
                         request.abort();
                         abort();
                     }
                 };
-            },
-            revert: null, // Prevent FilePond from sending a DELETE request
-        
+            }
         }
     });
 
-    // No need to handle form submission here as FilePond handles file upload
+    document.getElementById('uploadForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        // Manually process each file
+        pond.getFiles().forEach(fileItem => {
+            pond.processFile(fileItem.id).then(file => {
+                console.log('File uploaded', file);
+            }).catch(error => {
+                console.error('File upload error', error);
+            });
+        });
+    });
 });
